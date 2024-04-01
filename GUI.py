7cc8363747ba6759facebadde61607d60fe1b6b3
Jorge Magno <jorgejapao00@gmail.com>
2024-04-01 08:16:29 -0300

    GUI.py updated

--- GUI.py
+++ GUI.py
@@ -1,9 +1,19 @@
-from short import shorturl
 import tkinter
+from short import shorturl
 
-def Exemplo():
+
+def gui():
     root = tkinter.Tk()
-    root.title("Simple URL Shortner")
-    root.resizable(False, False)
+    root.title("Simple URL Shortener")
+    root.resizable(True, True)
 
-Exemplo()
+
+    quit = tkinter.Button(root, text="QUIT", command=root.destroy)
+    quit.pack()
+
+    root.iconify() #Minimiza a tela
+    root.update()
+    root.deiconify() #Maximiza a tela
+    root.mainloop()  #loop principal, impede o código de seguir e permite capturar inputs
+
+gui()
