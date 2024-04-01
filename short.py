0d4ed349004b2a42705ac736e2c4961343069048
Jorge Magno <jorgejapao00@gmail.com>
2024-04-01 08:22:26 -0300

    Functions separated from each other. One for asking the link and the other to print the link after shortening it.

--- short.py
+++ short.py
@@ -2,8 +2,8 @@ import gdshortener
 
 s = gdshortener.ISGDShortener()
 
-def shorturl():
-    tobes=str(input("Qual a url que você gostaria de encurtar?"))
-    print (s.shorten(tobes))
-
-shorturl()
+def tbs():
+    tbs.tobes=str(input("Qual a url que você gostaria de encurtar?"))
+    
+def shot():
+    print (s.shorten(tbs.tobes))
