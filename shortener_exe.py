5f173a231632af9e0f66ad6649b98668516cf8cc
Jorge Magno <jorgejapao00@gmail.com>
2024-04-02 21:13:38 -0300

    Try-except added to the main part of the code

--- shortener_exe.py
+++ shortener_exe.py
@@ -12,15 +12,18 @@ def tbs():
     tbs.tobes=str(input("Qual link gostaria de encurtar? "))
     
 def short():
-    final=(str(s.shorten(tbs.tobes)))
-    url = r"[^,()'']+"
-    correspondencia = re.search(url, final)
-    if correspondencia :
-        final_url = correspondencia.group()
-        pyperclip.copy(final_url)
-        print("O link encurtado foi copiado para a área de transferência! ")
-    else:
-        print('Erro')
+    try:
+        final=(str(s.shorten(tbs.tobes)))
+        url = r"[^,()'']+"
+        correspondencia = re.search(url, final)
+        if correspondencia :
+            final_url = correspondencia.group()
+            pyperclip.copy(final_url)
+            print("O link encurtado foi copiado para a área de transferência! ")
+        else:
+            print('Erro')
+    except Exception as e:
+        print(f"Um erro inesperado ocorreu: {e}")
 
 
 def novo():
