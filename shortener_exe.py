56e2e7b7d5110d98db1ea06d7f57fffc4f964aa0
Jorge Magno <jorgejapao00@gmail.com>
2024-04-02 21:35:40 -0300

    Minor update on shortener_exe.py

--- shortener_exe.py
+++ shortener_exe.py
@@ -4,13 +4,9 @@ import pyperclip
 import re
 from art import *
 
-
-
 s = gdshortener.ISGDShortener()
 n="Gostaria de encurtar outro link? (S/N) "
 text=text2art("Simple Python URL Shortener - By horue")
-
-print()
 
 def tbs():
     tbs.tobes=str(input("Qual link gostaria de encurtar? "))
@@ -36,6 +32,7 @@ def novo():
         sys.exit()
     else:
         return
+
 
 print(text)        
 while True:
